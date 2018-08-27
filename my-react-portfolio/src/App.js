9bf452d2751acd7a1e2710b309cd6dbbabb0c291
Alex Jivov <alexjivov@gwu.edu>
2018-08-27 19:36:49 -0400

    added header component copy from first-react-website as a shell, as well as homepage shell

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
 
-
-
+// REACT ROUTER - Allows navigation of different webpages - npm react-router-dom
+import {
+  BrowserRouter as Router,
+  Route, 
+  Link
+  
+} from 'react-router-dom';
 
 //import minified CSS
 import './Assets/css/default.min.css';
---
+++ my-react-portfolio/src/Components/headerComponent/header.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+// Removed CSS because it is already included in App.js
+
+//Imports JUST the link for use
+// In React, you do not need anchor tags because they are single page applications
+// Use <Link /> instead of <a> 
+import {
+    Link
+    
+  } from 'react-router-dom';
+
+// components
+// have to rename component in here and at export - must start with a capital
+class Header extends Component {
+  render() {
+    return (
+     <header>
+        <div className="logo">
+        LOGO
+        </div>
+
+        <nav>
+            <ul>
+                <li className="first">
+                <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/Portfolio">Products</Link>
+                </li>
+                <li className ="last">
+            
+                <Link to="/Contact">Contact</Link>
+                </li>
+
+            </ul>
+        </nav>
+
+    </header>
+    );
+  }
+}
+
+export default Header;
