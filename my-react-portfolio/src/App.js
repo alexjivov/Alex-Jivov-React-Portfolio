8f04cadf848e596d4e5b088aa3d8ea128aaf0738
Alex Jivov <alexjivov@gwu.edu>
2018-08-29 21:13:29 -0400

    added react MDL UI framework for preset layouts - commented out existing components for now

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -9,22 +9,40 @@ import './Assets/css/default.min.css';
 ////COMPONENTS////
 /////////////////
 
+import {Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 // import header component
-import Header from './Components/headerComponent/header';
-import Homepage from './Components/pages/homepage';
-import Biobox from './Components/boxComponents/biobox';
-import OngoingProjects from './Components/boxComponents/ongoingprojects';
+// import Header from './Components/headerComponent/header';
+// import Homepage from './Components/pages/homepage';
+// import Biobox from './Components/boxComponents/biobox';
+// import OngoingProjects from './Components/boxComponents/ongoingprojects';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-
-        <Homepage />
-        <Biobox />
-        <OngoingProjects />
+       <div style={{height: '300px', position: 'relative'}}>
+    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+        <Header transparent title="Title" style={{color: 'white'}}>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
+        
       </div>
     );
   }
--- my-react-portfolio/src/index.js
+++ my-react-portfolio/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
