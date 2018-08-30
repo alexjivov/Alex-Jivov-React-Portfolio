6a843b6bbcba577300e1b93102d13741612788d2
Alex Jivov <alexjivov@gwu.edu>
2018-08-30 09:04:56 -0400

    changed react-mdl layout and imported main.js successfully into App.js

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 
 // REACT ROUTER - Allows navigation of different webpages - npm react-router-dom
-import {
-  BrowserRouter as Router,
-  Route, 
-  Link
-  
-} from 'react-router-dom';
+
 
 
 //import minified CSS
@@ -16,35 +11,37 @@ import './Assets/css/default.min.css';
 ////COMPONENTS////
 /////////////////
 
-import {Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
+import {Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Homepage from './Components/pages/homepage';
+import Main from './Components/main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <div style={{height: '300px', position: 'relative'}}>
-    <Layout style={{background:  'url("./src/Assets/Img/Portfolio copy 2.png") center / cover'}}>
-        <Header transparent title="Title" style={{color: 'white'}}>
+    <Layout fixedHeader>
+        <Header title={<span><span style={{ color: '#ddd' }}> </span><strong></strong></span>}>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
             </Navigation>
         </Drawer>
         <Content />
     </Layout>
 </div>
-        
+      <Main />
       </div>
     );
   }
--- my-react-portfolio/src/Components/main.js
+++ my-react-portfolio/src/Components/main.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 
+import Homepage from './pages/homepage';
+
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={Homepage} />
+    </Switch>
+)
+
+export default Main;
