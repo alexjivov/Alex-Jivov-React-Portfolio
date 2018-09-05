ee111aa32fbd616cb4443bfaf25fcc2a5261e241
Alex Jivov <alexjivov@gwu.edu>
2018-09-04 20:21:12 -0400

    created tabs constructor for portfolio - stuck behind nav for some reason

--- my-react-portfolio/src/Components/pages/portfolio.js
+++ my-react-portfolio/src/Components/pages/portfolio.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 //importing react-mdl components (i.e. Grid)
-import {Grid,Cell} from 'react-mdl';
-
+import {Tabs, Tab} from 'react-mdl';
 
 // components
 // have to rename component in here and at export - must start with a capital
 class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 4};
+  }
+
   render() {
     return (
-     <div style={{width: '100%', margin: 'auto'}}>
-        <Grid className="landing-grid">
-        <Cell col={12}></Cell>
-
-        </Grid>
+     <div className="category-tabs">
+      <Tabs activeTab ={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+      <Tab>React</Tab>
+      <Tab>JavaScript</Tab>
+      <Tab>mySQL</Tab>
+      <Tab>MongoDB</Tab>
+      
+      </Tabs>
     </div>
     );
   }
