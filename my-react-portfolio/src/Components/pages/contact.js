ed0da018a05f29bf82aeb00cf5abe4519bdfd0b0
Alex Jivov <alexjivov@gwu.edu>
2018-09-09 12:02:04 -0400

    finished shell of contact page - need to fill with my own content

--- my-react-portfolio/src/Components/pages/contact.js
+++ my-react-portfolio/src/Components/pages/contact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // Removed CSS because it is already included in App.js
 // Import grid system from react-mdl
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 // components
 // have to rename component in here and at export - must start with a capital
 class Contact extends Component {
@@ -17,10 +17,41 @@ class Contact extends Component {
               style={{ height: '250px' }}
 
             />
-            <p style={{width:'75%', margin: 'auto',paddingTop:'1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
           </Cell>
-          <Cell col={6}>half</Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (647)-624-4362
+                  </ListItemContent>
+                </ListItem>
+
+
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    jivovalexander@gmail.com
+                  </ListItemContent>
+                </ListItem>
+
+
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-skype" aria-hidden="true" />
+                    (647)-624-4362
+                  </ListItemContent>
+                </ListItem>
+              </List>
+
+            </div>
+
+          </Cell>
         </Grid>
 
       </div>
