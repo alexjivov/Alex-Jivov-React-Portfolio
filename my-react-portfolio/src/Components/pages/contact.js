226d84fb157c0e0d974e162b9b70ac2162779be6
Alex Jivov <alexjivov@gwu.edu>
2018-09-09 11:38:57 -0400

    dummy avatar and shell for left side page grid

--- my-react-portfolio/src/Components/pages/contact.js
+++ my-react-portfolio/src/Components/pages/contact.js
@@ -7,13 +7,23 @@ import { Grid, Cell } from 'react-mdl';
 class Contact extends Component {
   render() {
     return (
-     <div className = "contact-body">
-      <Grid className ="contact-grid">
-        <Cell col={6}>half</Cell>
-        <Cell col={6}>half</Cell>
-      </Grid>
-      
-    </div>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Alexander Jivov</h2>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{ height: '250px' }}
+
+            />
+            <p style={{width:'75%', margin: 'auto',paddingTop:'1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+          </Cell>
+          <Cell col={6}>half</Cell>
+        </Grid>
+
+      </div>
     );
   }
 }
