90c5c531232ce1de02c2383d7c4ef6c50aa95ee7
Alex Jivov <alexjivov@gwu.edu>
2018-09-14 18:19:10 -0400

    aos integration complete - find the proper component mount function in homepage.js

--- my-react-portfolio/src/Components/pages/homepage.js
+++ my-react-portfolio/src/Components/pages/homepage.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 // Removed CSS because it is already included in App.js
 import { Grid, Cell } from 'react-mdl';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-// components
-// have to rename component in here and at export - must start with a capital
+
 class Homepage extends Component {
+
+//AOS - Need to mount AOS BEFORE render in the below function to make it work
+    componentDidMount(){
+        AOS.init({
+          duration : 2000
+        })
+      
+    }
     render() {
         return (
-            <div style={{ width: '100%', margin: 'auto' }}>
-                <Grid className="landing-grid">
+            <div  style={{ width: '100%', margin: 'auto' }}>
+                <Grid  className="landing-grid">
                     <Cell col={12}>
                     
-                    <div className="banner-text">
+                    <div  data-aos="fade-right" className="banner-text">
                         <h1> Alexander Jivov </h1>
                         <h1>Full Stack Web Developer</h1>
 
